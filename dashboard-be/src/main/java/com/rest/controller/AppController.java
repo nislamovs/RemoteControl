261ea7e77c2dfefa1a51d5e4c2eb6c75f4f32d8b
you@example.com <you@example.com>
2017-09-14 18:23:56 +0300

    Few fixes regarding probecheck

--- dashboard-be/src/main/java/com/rest/controller/AppController.java
+++ dashboard-be/src/main/java/com/rest/controller/AppController.java
@@ -1,9 +1,12 @@
 package com.rest.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AppController {
@@ -20,7 +23,8 @@ public class AppController {
     }
 
     @RequestMapping("/healthcheck")
-    String healthCheck() {
-        return "SpringBootCRUDApp:  v.1.0.0 \nStatus : OK";
+    @ResponseBody
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.status(HttpStatus.OK).body("<p>SpringBootCRUDApp:  v.1.0.0</p><p>Status : OK</p>");
     }
 }
