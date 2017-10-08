0194d86cb9951b9caee9f11022d5ad99555d26be
Nizami Islamovs <n_islamovs@inbox.lv>
2017-10-08 23:25:18 +0300

    Added bower and grunt-cli to dev-dependencies. Fixed build script. Added git-ignore for better dev.experience.

--- maintenance_scripts/deploy.sh
+++ maintenance_scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo -en "Deploying..."
-rsync ../target/java.test-1.0-SNAPSHOT.jar /home/perkele/Desktop/
+rsync ../target/java.test-1.0-SNAPSHOT.jar /home/user/Desktop/
 echo -e "done!";
